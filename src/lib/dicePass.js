58ae4bcdf5c100c5f0628cb289379e17626c8ede
Sarah Koehler <sarahk@magnatag.com>
2022-08-11 16:26:31 -0400

    solved separator input not being reflected in memorable password

--- src/lib/dicePass.js
+++ src/lib/dicePass.js
@@ -4,6 +4,6 @@ const dicePass = (length, separator) => new Array(parseInt(length)).fill(null).m
 	const key = new Array(5).fill(null).map(() => Math.round(Math.random() * 5) + 1).join('');
 	const {word} = wordList.find(l => l.key === key);
 	return word;
-}).join(';');
+}).join(separator);
 
 export default dicePass;
