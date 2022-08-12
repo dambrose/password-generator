dabf6c2469ee9a8651bc0f9d214dc5e0152857a7
Dennis Ambrose <dennisa@magnatag.com>
2022-08-12 17:01:46 -0400

    randomPass

--- src/lib/randomPass.js
+++ src/lib/randomPass.js
@@ -1,2 +1,29 @@
-const randomPass = (length) => new Array(parseInt(length)).fill().map(() => String.fromCharCode(33 + Math.round(Math.random() * (126 - 33)))).join('');
-export default randomPass;
+const ALPHA = 'abcdefghijklmnopqrstuvwxyz';
+const NUMBERS = '0123456789';
+const SPECIAL1 = '!"#$%&\'()*+,-./';
+const SPECIAL2 = ':;<=>?@';
+
+export default function (length, {
+	lowercase = true,
+	uppercase = true,
+	numeric = true,
+	specialAll = true,
+	special1 = true,
+	special2 = true,
+	ambiguous = false
+} = {}) {
+
+	const chars = (lowercase ? ALPHA : '') +
+		(uppercase ? ALPHA.toUpperCase() : '') +
+		(numeric ? NUMBERS : '') +
+		(specialAll || special1 ? SPECIAL1 : '') +
+		(specialAll || special2 ? SPECIAL2 : '')
+			.split('')
+			.filter(char => !ambiguous || !/l1IoO0/.test(char));
+
+	return new Array(length).fill(null).map(() => {
+		const randIndex = Math.random() * chars.length;
+		return chars[randIndex];
+	}).join('');
+
+}
