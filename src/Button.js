565a2463dd7ed1a85f1a7ee717ef5c69d3986604
Dennis Ambrose <dennisa@magnatag.com>
2022-08-13 23:01:44 -0400

    just a button

--- src/Button.js
+++ src/Button.js
@@ -1,21 +1,8 @@
-export default function Button({children, onClickButton, className, id}) {
+export default function Button({children, onClick}) {
 
-    return (
-
-        <div class="d-grid gap-2 col-6 mx-auto">
-            <button type="button" className="btn btn-primary"
-                    id={id}
-                    onClick={onClickButton}
-            >
-                {children}
-            </button>
-        </div>
-
-        /*<button className={className}
-                id={id}
-                onClick={onClickButton}
-        >
-            {label}
-        </button>*/
-    );
+	return (
+		<button className="btn btn-primary" onClick={onClick}>
+			{children}
+		</button>
+	);
 }
