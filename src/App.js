bd54e0ee38e52acb7b6082b7f88204a88b91b681
Dennis Ambrose <dennisa@magnatag.com>
2022-08-13 23:02:26 -0400

    h1 in header

--- src/App.js
+++ src/App.js
@@ -3,18 +3,16 @@ import Random from './Random';
 import Memorable from './Memorable';
 
 export default function App() {
-
-	const title = 'Password Generator!';
-	const items = [
-		{title: 'Random', Component: Random},
-		{title: 'Memorable', Component: Memorable}
-	];
-
 	return (
 		<div className="root">
-			<header className="d-flex justify-content-center fs-3">{title}</header>
+			<header className="d-flex justify-content-center fs-3">
+				<h1 className="h2">Password Generator</h1>
+			</header>
 			<div className="d-flex justify-content-center m-3">
-				<Switcher items={items}/>
+				<Switcher items={[
+					{title: 'Random', Component: Random},
+					{title: 'Memorable', Component: Memorable}
+				]}/>
 			</div>
 		</div>
 	);
