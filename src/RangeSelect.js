264aba3725e50693e999b766cfabda2e3c82187a
Dennis Ambrose <dennisa@magnatag.com>
2022-08-13 23:01:08 -0400

    remove label

--- src/RangeSelect.js
+++ src/RangeSelect.js
@@ -1,20 +1,12 @@
-import {useRef} from 'react';
-
 export default function RangeSelect({min = 0, max = 100, value, setValue}) {
-	const id = useRef(`range_${(Math.random() * 10000000).toFixed(0)}`);
-
 	return (
-		<>
-			<label htmlFor={id.current} className="form-label d-flex justify-content-center">Example range</label>
-            <input
-                id={id.current}
-                type="range"
-                className="form-range"
-                max={max}
-                min={min}
-                value={value}
-                onChange={({target: {value}}) => setValue(value)}
-            />
-		</>
+		<input
+			type="range"
+			className="form-range"
+			max={max}
+			min={min}
+			value={value}
+			onChange={({target: {value}}) => setValue(value)}
+		/>
 	);
 }
